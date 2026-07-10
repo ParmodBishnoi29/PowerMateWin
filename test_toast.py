b7eaa33327e2e4f42b1cc14f3a30c010aaa006c7
Boy.Toy <bparmod039@gmail.com>
2026-07-11 04:45:57 +0530

    Remove test_toast.py test file

--- test_toast.py
+++
@@ -1,9 +0,0 @@
-from winotify import Notification
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-toast = Notification(app_id="powermate.win.bridge",
-                     title="Test",
-                     msg="Click me to open notifications",
-                     icon=os.path.join(BASE_DIR, "icon.ico"),
-                     launch="ms-settings:notifications")
-toast.show()
